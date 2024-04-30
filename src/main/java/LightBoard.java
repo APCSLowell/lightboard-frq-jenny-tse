28ae092d6db084a32d5c74d6714371d9a1ccd3f2
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-04-29 19:13:47 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -14,7 +14,7 @@ public class LightBoard
     for(int r=0;r<numRows;r++){
       for(int c=0;c<numCols;c++){
         double rnd=Math.random();
-        lights[r][c]=rnc<0.4;
+        lights[r][c]=rnd<0.4;
       }
     }
 
