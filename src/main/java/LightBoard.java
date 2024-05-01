70740147a6ba1174cb79536965b91d5a78657fea
Jenny Tse <142839347+jenny-tse@users.noreply.github.com>
2024-05-01 09:39:14 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -8,21 +8,35 @@ public class LightBoard
    * Precondition: numRows > 0, numCols > 0
    * Postcondition: each light has a 40% probability of being set to on.
    */
-  public LightBoard(int numRows, int numCols)
-  {
-    /* to be implemented in part (a) */
-
+  public LightBoard(int numRows, int numCols){
+  /* to be implemented in part (a) */
+  lights = new boolean [numRows][numCols];
+    for(int r=0;r<numRows;r++){
+      for(int c=0;c<numCols;c++){
+        double rnd=Math.random();
+        lights[r][c]=rnd<0.4;
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
    *  as described in part (b).
    *  Precondition: row and col are valid indexes in lights.
    */
-  public boolean evaluateLight(int row, int col)
-  {
-    /* to be implemented in part (b) */
-   
- 
+  public boolean evaluateLight(int row, int col){
+int numOn=0;
+    for(int r=0; r<lights.length;r++){
+      if(lights[r][col]){
+        numOn++;
+      }
+    }
+    if(lights[row][col]&&numOn%2==0){
+      return false;
+    }
+    if(!lights[row][col]&&numOn%3==0){
+      return true;
+    }
+    return lights[row][col];
   }
   public boolean[][] getLights()
   {
